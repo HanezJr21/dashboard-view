5d3294f227f27550c39ef37ec8b39839b31b8730
HanezJr21 <yohanesrinaldy06@gmail.com>
2021-07-04 23:59:04 +0700

    5 files modified

--- app/sidenav-content/sidenav-content.component.ts
+++ app/sidenav-content/sidenav-content.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../_services';
+import { User } from '../_models';
 
 @Component({
   selector: 'app-sidenav-content',
@@ -6,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-content.component.css']
 })
 export class SidenavContentComponent implements OnInit {
-  constructor() {}
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      x => (this.currentUser = x)
+    );
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 
   ngOnInit() {}
 }
